841e6df2cd5ba60bb41285fce3326fc460076834
bluelsa <elsa22745@gmail.com>
2023-05-28 12:49:39 +0800

    Update server

---
+++ src/api/auth.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+const authUrl = 'https://todo-list.alphacamp.io/api/auth';
+
+export const login = async ({ username, password }) => {
+  try {
+    const { data } = await axios.post(`${authUrl}/login`, {
+      username,
+      password,
+    });
+    // console.log(data)
+    const { authToken } = data;
+    if (authToken) {
+      return { success: true, ...data };
+    }
+    return data;
+  } catch (error) {
+    console.error('[Login Failed]:', error);
+  }
+};
+
+export const register = async ({ username, email, password }) => {
+  try{
+    const { data } = await axios.post(`${authUrl}/register`, {
+      username,
+      email,
+      password
+    })
+    // console.log(data)
+    const { authToken } = data;
+    if (authToken) {
+      return { success: true, ...data };
+    }
+    return data;
+  } catch (error) {
+    console.error('[Register Failed]:', error)
+  }
+}
+
+export const checkPermission = async(authToken) => {
+  try {
+    const res = await axios.get(`${authUrl}/test-token`, {
+      headers: {
+        Authorization: 'Bearer ' + authToken
+      }
+    })
+    return res.data.success
+  } catch (error) {
+    console.error('[Check Permission Failed]:', error)
+  }
+}
